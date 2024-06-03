1b2be3d78d0155b5373f71db0970114cef1ac45f
lauravivan <lauvivang@gmail.com>
2024-06-03 18:17:06 -0300

    Add exportation file

---
+++ src/util/index.ts
@@ -0,0 +1,17 @@
+export {
+  setTheme,
+  getTheme,
+  setCardsView,
+  getCardsView,
+} from "@/util/localStorage";
+
+export {
+  DEFAULT_THEME,
+  TOGGLE_THEME,
+  DEFAULT_VIEW,
+  TOGGLE_VIEW,
+  FILTER,
+  SORT,
+  FILTER_OPTIONS,
+  SORT_OPTIONS,
+} from "@/util/variable";
