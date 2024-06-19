d7fadbef48e75760a112a0ead4321f655c8f0f5c
lauravivan <lauvivang@gmail.com>
2024-06-19 19:39:52 -0300

    Removed old logic

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,29 +1,3 @@
-// /* eslint-disable react/prop-types */
-// import "@/components/Modal/index.css";
-// import { useRef, useEffect } from "react";
-// import RemoveIcon from "uiEl/RemoveIcon/index.jsx";
-
-// export function Modal({
-//   isClosed,
-//   children,
-//   styles,
-//   handleModalClose,
-//   classes,
-// }) {
-//   function handleRemoveClick(e) {
-//     e.stopPropagation();
-//     handleModalClose();
-//   }
-
-//   return createPortal(
-//     <dialog open={!isClosed} className={`modal ${classes}`} style={styles}>
-//       <RemoveIcon onClick={handleRemoveClick} />
-//       {children}
-//     </dialog>,
-//     document.body
-//   );
-// }
-
 import { createPortal } from "react-dom";
 import { useModal } from "@/context";
 
@@ -36,7 +10,6 @@ export function Modal({ children }: ModalType) {
 
   return createPortal(
     <dialog open={modal.isOpen} className="modal">
-      {/* <RemoveIcon onClick={modal.openModal} /> */}
       {children}
     </dialog>,
     document.body
