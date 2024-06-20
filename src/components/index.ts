ae2558668d677479eeb3aa76dda6f9f546164175
lauravivan <lauvivang@gmail.com>
2024-06-20 16:02:12 -0300

    Remove RemoveIcon component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export { Card } from "@/components/Card";
 export { Header } from "@/components/Header";
 export { Main } from "@/components/Main";
 export { Modal } from "@/components/Modal";
-export { RemoveIcon } from "@/components/RemoveIcon";
 export { SelectBtn } from "@/components/SelectBtn";
 export { ToggleBtn } from "@/components/ToggleBtn";
