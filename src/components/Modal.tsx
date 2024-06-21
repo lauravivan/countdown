6f24ac2bb03d0f6cd1609e4397a8b35396547d35
lauravivan <lauvivang@gmail.com>
2024-06-21 17:17:29 -0300

    Add modal content

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,18 @@ export function Modal({ children }: ModalType) {
 
   return createPortal(
     <dialog open={modal.isOpen} className="modal">
-      {children}
+      <div className="modal__content">
+        <div className="modal__content__header">
+          <h3 className="modal__content__header__title">{modal.modalTitle}</h3>
+          <button
+            className="modal__content__header__close"
+            onClick={() => modal.closeModal()}
+          >
+            <ion-icon name="close-outline"></ion-icon>
+          </button>
+        </div>
+        {children}
+      </div>
     </dialog>,
     document.body
   );
