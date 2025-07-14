6c04dbb66ed71a090763f02c80a038db5aec534b
lauravivan <lauvivang@gmail.com>
2025-07-14 20:06:09 -0300

    Delete files

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Card } from "@/components/Card";
-export { Header } from "@/components/Header";
-export { Main } from "@/components/Main";
-export { Modal } from "@/components/Modal";
-export { SelectBtn } from "@/components/SelectBtn";
-export { ToggleBtn } from "@/components/ToggleBtn";
