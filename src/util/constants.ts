7e4e393fcf404cbec26b2b9b39818aa40767bc76
lauravivan <lauvivang@gmail.com>
2025-07-14 16:14:33 -0300

    Change variable to constants

---
+++ src/util/constants.ts
@@ -0,0 +1,32 @@
+export const DEFAULT_THEME = "sunny";
+
+export const TOGGLE_THEME = "moon";
+
+export const DEFAULT_VIEW = "grid";
+
+export const TOGGLE_VIEW = "list";
+
+export const FILTER = "filter-sharp";
+
+export const SORT = "swap-vertical-sharp";
+
+export const FILTER_OPTIONS = [
+  "All",
+  "Happening this month",
+  "Happening next month",
+  "Happening in 2 months",
+  "Happening in 3 months",
+  "Happenning in 4 months",
+  "Hapenning in 5 months",
+  "Happening in 6 months",
+  "Happening in more than 6 months",
+];
+
+export const SORT_OPTIONS = [
+  "By Creation (default)",
+  "By Color",
+  "By Alphabet",
+  "By Date",
+];
+
+export const DESC_MAX_LENGTH = 150;
